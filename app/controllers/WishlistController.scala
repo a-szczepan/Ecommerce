3b6f03b4fb4899b6191647e02f24b2a279801f35
a-szczepan <szczepanczykangelika@gmail.com>
2021-05-12 06:37:04 +0200

    updated wishlist controllers + routes fixed

--- app/controllers/WishlistController.scala
+++ app/controllers/WishlistController.scala
@@ -1,35 +1,73 @@
 package controllers
 
+import models.{Wishlist, WishlistRepository, UserRepository, ProductRepository}
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
-
 import javax.inject._
 import scala.concurrent.{ExecutionContext, Future}
 
-
 @Singleton
-class WishlistController @Inject()(cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc){
+class WishlistController @Inject()(wishlistRepository: WishlistRepository,
+                                   userRepository: UserRepository,
+                                   productRepository: ProductRepository,
+                                cc: ControllerComponents)
+                               (implicit ec: ExecutionContext)
+  extends AbstractController(cc){
 
-  def addToWishlist: Action[AnyContent] = Action.async {
-    Future{
-      Ok("ok")
+  def createWishlist: Action[JsValue] = Action.async(parse.json) {
+    implicit request =>
+      request.body.validate[Wishlist].map {
+        wishlist =>
+          wishlistRepository.create(wishlist.user_id, wishlist.product_id).map { res =>
+            Ok(Json.toJson(res))
+          }
+      }.getOrElse(Future.successful(BadRequest("")))
+  }
+
+  def getWishlists: Action[AnyContent] = Action.async {
+    val wishlists = wishlistRepository.list()
+    wishlists.map{
+      wishlists => Ok(Json.toJson(wishlists))
     }
   }
 
-  def getWishlist: Action[AnyContent] = Action.async {
-    Future{
-      Ok("")
+  def getWishlist(id: Int): Action[AnyContent] = Action.async {
+    val wishlist = wishlistRepository.getById(id)
+    wishlist.map {
+      case Some(res) => Ok(Json.toJson(res))
+      case None => NotFound("")
     }
   }
 
-  def updateWishlist(id: Long): Action[AnyContent] = Action async {
-    Future{
-      Ok("")
+  def updateWishlist(id: Int): Action[JsValue] = Action.async(parse.json) { request =>
+    request.body.validate[Wishlist].map {
+      wishlist =>
+        wishlistRepository.update(wishlist.id, wishlist).map { res =>
+          Ok(Json.toJson(res))
+        }
+    }.getOrElse(Future.successful(BadRequest("")))
+  }
+
+  def deleteWishlist(id: Int): Action[AnyContent] = Action.async {
+    wishlistRepository.delete(id).map { res =>
+      Ok(Json.toJson(res))
     }
   }
 
-  def deleteFromWishlist(id: Long): Action[AnyContent] = Action {
-    NoContent
+  def getWishlistByUser(user_id: Int): Action[AnyContent] = Action.async{
+    val wishlists = wishlistRepository.getByUser(user_id)
+    wishlists.map {
+      wishlists => Ok(Json.toJson(wishlists))
+    }
   }
+
+  def getWishlistByProduct(product_id: Int): Action[AnyContent] = Action.async{
+    val wishlists = wishlistRepository.getByProduct(product_id)
+    wishlists.map {
+      wishlists => Ok(Json.toJson(wishlists))
+    }
+  }
+
 
 }
 
--- app/controllers/WishlistFormController.scala
+++ app/controllers/WishlistFormController.scala
@@ -16,6 +16,96 @@ class WishlistFormController @Inject()(wishlistRepository: WishlistRepository,
                                      (implicit ec: ExecutionContext)
   extends MessagesAbstractController(cc) {
 
+  var userList: Seq[User] = Seq[User]()
+  var productList: Seq[Product] = Seq[Product]()
+
+  userRepository.list().onComplete {
+    case Success(user) => userList = user
+    case Failure(e) => print("", e)
+  }
+
+  productRepository.list().onComplete {
+    case Success(product) => productList = product
+    case Failure(e) => print("", e)
+  }
+
+  val wishlistForm: Form[CreateWishlistForm] = Form {
+    mapping(
+      "user_id" -> number,
+      "product_id" -> number,
+    )(CreateWishlistForm.apply)(CreateWishlistForm.unapply)
+  }
+
+  val updateWishlistForm: Form[UpdateWishlistForm] = Form {
+    mapping(
+      "id" -> number,
+      "user_id" -> number,
+      "product_id" -> number,
+    )(UpdateWishlistForm.apply)(UpdateWishlistForm.unapply)
+  }
+
+  /* -------------------------------------------- */
+
+  def updateWishlist(id: Int): Action[AnyContent] = Action.async { implicit request: MessagesRequest[AnyContent] =>
+    val wishlist = wishlistRepository.getById(id)
+    wishlist.map(wishlist => {
+      val wishlistForm = updateWishlistForm.fill(UpdateWishlistForm(wishlist.get.id, wishlist.get.user_id, wishlist.get.product_id))
+      Ok(views.html.wishlist.updateWishlist(wishlistForm, userList, productList))
+    })
+  }
+
+  def updateWishlistHandle(): Action[AnyContent] = Action.async { implicit request =>
+    updateWishlistForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(
+          BadRequest(views.html.wishlist.updateWishlist(errorForm, userList, productList))
+        )
+      },
+      wishlist => {
+        wishlistRepository.update(wishlist.id, Wishlist(wishlist.id, wishlist.user_id, wishlist.product_id)).map { _ =>
+          Redirect("/wishlist/all")
+        }
+      }
+    )
+  }
+
+  def deleteWishlist(id: Int): Action[AnyContent] = Action {
+    wishlistRepository.delete(id)
+    Redirect("/wishlist")
+  }
+
+  def allWishlists: Action[AnyContent] = Action.async { implicit request =>
+    wishlistRepository.list().map(wishlist => Ok(views.html.wishlist.allWishlists(wishlist)))
+  }
+
+  def getById(id: Int) = Action.async { implicit request =>
+    val wishlist = wishlistRepository.getById(id)
+    wishlist.map(wishlist => wishlist match {
+      case Some(p) => Ok(views.html.wishlist.wishlistById(p))
+      case None => Redirect(routes.WishlistFormController.allWishlists)
+    })
+  }
+
+  def createWishlist() = Action { implicit request =>
+    val users = Await.result(userRepository.list(), Duration.Inf)
+    val products = Await.result(productRepository.list(), Duration.Inf)
+    Ok(views.html.wishlist.createWishlist(wishlistForm, users, products))
+  }
+
+  def createWishlistHandle(): Action[AnyContent] = Action.async { implicit request =>
+    wishlistForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(
+          BadRequest(views.html.wishlist.createWishlist(errorForm, userList, productList))
+        )
+      },
+      wishlist => {
+        wishlistRepository.create(wishlist.user_id, wishlist.product_id).map { _ =>
+          Redirect("/wishlist/all")
+        }
+      }
+    )
+  }
 
 }
 case class CreateWishlistForm(user_id: Int, product_id: Int)
