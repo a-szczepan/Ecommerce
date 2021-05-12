647141ab85d36204f6f198d6b0799a410b3fad1b
a-szczepan <szczepanczykangelika@gmail.com>
2021-05-12 05:50:58 +0200

    updated shipping controllers + routes fixed

--- app/controllers/ShippingController.scala
+++ app/controllers/ShippingController.scala
@@ -4,32 +4,52 @@ import play.api.mvc._
 
 import javax.inject._
 import scala.concurrent.{ExecutionContext, Future}
-
+import models.{Shipping, ShippingRepository}
+import play.api.libs.json.{JsValue, Json}
 
 @Singleton
-class ShippingController @Inject()(cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc){
+class ShippingController @Inject()(shippingRepository: ShippingRepository,
+                                   cc: ControllerComponents)
+                                  (implicit ec: ExecutionContext)
+  extends AbstractController(cc){
 
-  def createShipping: Action[AnyContent] = Action.async {
-    Future{
-      Ok("ok")
+  def createShipping(): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[Shipping].map {
+      shipping =>
+        shippingRepository.create(shipping.street_name, shipping.building_number, shipping.apartment_number, shipping.postal_code, shipping.city).map { res =>
+          Ok(Json.toJson(res))
+        }
+    }.getOrElse(Future.successful(BadRequest("")))
+  }
+
+  def getShippings: Action[AnyContent] = Action.async {
+    val shippings = shippingRepository.list()
+    shippings.map { shippings =>
+      Ok(Json.toJson(shippings))
     }
   }
 
-  def getShipping: Action[AnyContent] = Action.async {
-    Future{
-      Ok("")
+  def getShipping(id: Int): Action[AnyContent] = Action async {
+    val shipping = shippingRepository.getById(id)
+    shipping.map {
+      case Some(res) => Ok(Json.toJson(res))
+      case None => NotFound("")
     }
   }
 
-
-  def updateShipping: Action[AnyContent] = Action async {
-    Future{
-      Ok("")
-    }
+  def updateShipping(id: Int): Action[JsValue] = Action.async(parse.json) { request =>
+    request.body.validate[Shipping].map {
+      shipping =>
+        shippingRepository.update(shipping.id, shipping).map { res =>
+          Ok(Json.toJson(res))
+        }
+    }.getOrElse(Future.successful(BadRequest("")))
   }
 
-  def deleteShipping: Action[AnyContent] = Action {
-    NoContent
+  def deleteShipping(id: Int): Action[AnyContent] = Action.async {
+    shippingRepository.delete(id).map { res =>
+      Ok(Json.toJson(res))
+    }
   }
 
 }
--- app/controllers/ShippingFormController.scala
+++ app/controllers/ShippingFormController.scala
@@ -1,4 +1,5 @@
 package controllers
+
 import models.{Shipping, ShippingRepository}
 import play.api.data.Form
 import play.api.data.Forms._
@@ -10,12 +11,92 @@ import scala.util.{Failure, Success}
 
 @Singleton
 class ShippingFormController @Inject()(shippingRepository: ShippingRepository,
-                                      cc: MessagesControllerComponents)
-                                     (implicit ec: ExecutionContext)
+                                       cc: MessagesControllerComponents)
+                                      (implicit ec: ExecutionContext)
   extends MessagesAbstractController(cc) {
 
+  val shippingForm: Form[CreateShippingForm] = Form {
+    mapping(
+      "street_name" -> nonEmptyText,
+      "building_number" -> number,
+      "apartment_number" -> number,
+      "postal_code" -> nonEmptyText,
+      "city" -> nonEmptyText,
+    )(CreateShippingForm.apply)(CreateShippingForm.unapply)
+  }
 
+  val updateShippingForm: Form[UpdateShippingForm] = Form {
+    mapping(
+      "id" -> number,
+      "street_name" -> nonEmptyText,
+      "building_number" -> number,
+      "apartment_number" -> number,
+      "postal_code" -> nonEmptyText,
+      "city" -> nonEmptyText,
+    )(UpdateShippingForm.apply)(UpdateShippingForm.unapply)
+  }
+
+  def updateShipping(id: Int): Action[AnyContent] = Action.async { implicit request: MessagesRequest[AnyContent] =>
+    val shipping = shippingRepository.getById(id)
+    shipping.map(shipping => {
+      val shippingForm = updateShippingForm.fill(UpdateShippingForm(shipping.get.id, shipping.get.street_name, shipping.get.building_number, shipping.get.apartment_number, shipping.get.postal_code, shipping.get.city))
+      Ok(views.html.shipping.updateShipping(shippingForm))
+    })
+  }
+
+  def updateShippingHandle(): Action[AnyContent] = Action.async { implicit request =>
+    updateShippingForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(
+          BadRequest(views.html.shipping.updateShipping(errorForm))
+        )
+      },
+      shipping => {
+        shippingRepository.update(shipping.id, Shipping(shipping.id, shipping.street_name, shipping.building_number, shipping.apartment_number, shipping.postal_code, shipping.city)).map { _ =>
+          Redirect("/shipping")
+        }
+      }
+    )
+  }
+
+  def deleteShipping(id: Int): Action[AnyContent] = Action {
+    shippingRepository.delete(id)
+    Redirect("/shipping")
+  }
+
+  def allShippings: Action[AnyContent] = Action.async { implicit request =>
+    shippingRepository.list().map(shippings => Ok(views.html.shipping.allShippings(shippings)))
+  }
+
+  def getById(id: Int) = Action.async { implicit request =>
+    val shipping = shippingRepository.getById(id)
+    shipping.map(shipping => shipping match {
+      case Some(p) => Ok(views.html.shipping.shippingById(p))
+      case None => Redirect(routes.ShippingFormController.allShippings)
+    })
+  }
+
+  def createShipping(): Action[AnyContent] = Action.async { implicit request: MessagesRequest[AnyContent] =>
+    val shippings = shippingRepository.list()
+    shippings.map(_ => Ok(views.html.shipping.createShipping(shippingForm)))
+  }
+
+  def createShippingHandle(): Action[AnyContent] = Action.async { implicit request =>
+    shippingForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(
+          BadRequest(views.html.shipping.createShipping(errorForm))
+        )
+      },
+      shipping => {
+        shippingRepository.create(shipping.street_name, shipping.building_number, shipping.apartment_number, shipping.postal_code, shipping.city).map { _ =>
+          Redirect("/shipping/all")
+        }
+      }
+    )
+  }
 }
+
 case class CreateShippingForm(street_name: String, building_number: Int, apartment_number: Int, postal_code: String, city: String)
 
 case class UpdateShippingForm(id: Int = 0, street_name: String, building_number: Int, apartment_number: Int, postal_code: String, city: String)
