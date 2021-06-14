7a99afba5b0a2ebcfc04a444fcd6dcc0b7b65321
a-szczepan <szczepanczykangelika@gmail.com>
2021-06-14 23:05:37 +0200

    sonar cleanup

--- react-app/src/components/Products.js
+++ react-app/src/components/Products.js
@@ -1,22 +1,9 @@
 import "../components/styles/Product.css";
 import { useEffect } from "react";
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
-import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import { connect, useDispatch, useSelector } from "react-redux";
 import {
-  addToCart,
-  addToWishlist,
-  fetchCart,
   fetchCategories,
   fetchProducts,
-  fetchWishlist,
 } from "../redux/Shopping/shopping-actions";
 import { ProductCard } from "./ProductCard";
 
